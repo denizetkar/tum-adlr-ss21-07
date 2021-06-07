0f1bebdcf4030f9199b1921f63574a383543beb4
Deniz Etkar <denizetkar@gmail.com>
2021-06-07 21:40:55 +0000

    fixed versions in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,12 +3,12 @@
 python3 -m venv venv
 source ./venv/bin/activate
 pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
-pip3 install gym
-pip3 install stable-baselines3
-pip3 install efficientnet_pytorch
-pip3 install tensorboard
+pip3 install gym==0.18.3
+pip3 install stable-baselines3==1.0
+pip3 install efficientnet-pytorch==0.7.1
+pip3 install tensorboard==2.5.0
 
-pip3 install atari-py
+pip3 install atari-py==0.2.9
 wget http://www.atarimania.com/roms/Roms.rar
 mkdir roms
 sudo apt-get install unar
