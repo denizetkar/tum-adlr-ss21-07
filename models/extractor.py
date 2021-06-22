984424bba22a0580ae12aa7e828403d026c5485d
Deniz Etkar <denizetkar@gmail.com>
2021-06-22 12:50:51 +0200

    added MLP extractor

--- models/extractor.py
+++ models/extractor.py
@@ -9,6 +9,7 @@ from stable_baselines3.common.utils import get_device
 from torch import nn
 from torchvision import models
 
+from .mlp import MLP
 from .rnn.lstms import LayerNormSemeniutaLSTM, MultiLayerLSTM
 
 
@@ -115,12 +116,94 @@ class RnnExtractor(nn.Module):
         return th.cat(policy_outputs, dim=0), th.cat(value_outputs, dim=0)
 
 
-class CnnExtractor(BaseFeaturesExtractor):
+class MlpExtractor(nn.Module):
+    """
+    Constructs an MLP that receives observations as an input and outputs a latent representation for the policy and
+    a value network. The ``net_arch`` parameter allows to specify the amount and size of the hidden layers and how many
+    of them are shared between the policy network and the value network. It is assumed to be a list with the following
+    structure:
+
+    1. An arbitrary length (zero allowed) number of integers each specifying the number of units in a shared layer.
+       If the number of ints is zero, there will be no shared layers.
+    2. An optional dict, to specify the following non-shared layers for the value network and the policy network.
+       It is formatted like ``dict(vf=[<value layer sizes>], pi=[<policy layer sizes>])``.
+       If it is missing any of the keys (pi or vf), no non-shared layers (empty list) is assumed.
+
+    For example to construct a network with one shared layer of size 55 followed by two non-shared layers for the value
+    network of size 255 and a single non-shared layer of size 128 for the policy network, the following layers_spec
+    would be used: ``[55, dict(vf=[255, 255], pi=[128])]``. A simple shared network topology with two layers of size 128
+    would be specified as [128, 128].
+
+    Adapted from Stable Baselines.
+
+    :param feature_dim: Dimension of the feature vector (can be the output of a CNN)
+    :param net_arch: The specification of the policy and value networks.
+        See above for details on its formatting.
+    :param activation_fn: The activation function to use for the networks.
+    :param device:
+    """
+
     def __init__(
-            self,
-            observation_space: gym.spaces.Space,
-            device: Union[th.device, str] = "auto",
-            features_dim: int = 512):
+        self,
+        feature_dim: int,
+        net_arch: List[Union[int, Dict[str, List[int]]]],
+        activation_fn: Type[nn.Module] = nn.PReLU,
+        device: Union[th.device, str] = "auto",
+    ):
+        super().__init__()
+        device = get_device(device)
+        shared_layers: List[int] = []
+        policy_layers: List[int] = []  # Layer sizes of the network that only belongs to the policy network
+        value_layers: List[int] = []  # Layer sizes of the network that only belongs to the value network
+
+        # Iterate through the shared layers and build the shared parts of the network
+        last_shared_dim = feature_dim
+        for layer in net_arch:
+            if isinstance(layer, int):  # Check that this is a shared layer
+                shared_layers.append(layer)
+                last_shared_dim = layer
+            else:
+                assert isinstance(layer, dict), "Error: the net_arch list can only contain ints and dicts"
+                if "pi" in layer:
+                    assert isinstance(layer["pi"], list), "Error: net_arch[-1]['pi'] must contain a list of integers."
+                    policy_layers = layer["pi"]
+                if "vf" in layer:
+                    assert isinstance(layer["vf"], list), "Error: net_arch[-1]['vf'] must contain a list of integers."
+                    value_layers = layer["vf"]
+                break  # From here on the network splits up in policy and value network
+
+        # Save dim, used to create the distributions
+        self.latent_dim_pi = policy_layers[-1] if len(policy_layers) > 0 else last_shared_dim
+        self.latent_dim_vf = value_layers[-1] if len(value_layers) > 0 else last_shared_dim
+
+        # Create networks
+        # If the list of layers is empty, the network will just act as an Identity module
+        self.shared_net = MLP(feature_dim, shared_layers, activation_fn=activation_fn, dropout=0.0).to(device)
+        self.policy_net = MLP(last_shared_dim, policy_layers, activation_fn=activation_fn, dropout=0.0).to(device)
+        self.value_net = MLP(last_shared_dim, value_layers, activation_fn=activation_fn, dropout=0.0).to(device)
+
+    def reset_parameters(self):
+        return
+
+    def reset_hiddens(self, batch_size: int = 1):
+        return
+
+    def reset_batch_hiddens(self, batch_idx: int):
+        return
+
+    def forward(self, features: th.Tensor, dones: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :param features: feature tensor of shape (seq_len, n_envs, feature_dim)
+        :param dones: Indicator for a new episode start of shape (seq_len, n_envs)
+        :return: latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        shared_latent = self.shared_net(features)
+        return self.policy_net(shared_latent), self.value_net(shared_latent)
+
+
+class CnnExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Space, device: Union[th.device, str] = "auto", features_dim: int = 512):
         super().__init__(observation_space, features_dim)
         device = get_device(device)
         assert is_image_space(observation_space), (
@@ -161,11 +244,7 @@ class CnnExtractor(BaseFeaturesExtractor):
 
 
 class EfficientNetExtractor(BaseFeaturesExtractor):
-    def __init__(
-            self,
-            observation_space: gym.spaces.Space,
-            device: Union[th.device, str] = "auto",
-            features_dim: int = 512):
+    def __init__(self, observation_space: gym.spaces.Space, device: Union[th.device, str] = "auto", features_dim: int = 512):
         super().__init__(observation_space, features_dim)
         device = get_device(device)
         assert is_image_space(observation_space), (
@@ -178,7 +257,7 @@ class EfficientNetExtractor(BaseFeaturesExtractor):
         )
         n_input_channels = observation_space.shape[0]
         assert n_input_channels == 3, "Number of input channels should be 3 for images"
-        self.efficient_net = EfficientNet.from_pretrained('efficientnet-b1', num_classes=features_dim).to(device)
+        self.efficient_net = EfficientNet.from_pretrained("efficientnet-b1", num_classes=features_dim).to(device)
 
     def forward(self, x: th.Tensor) -> th.Tensor:
         x = self.efficient_net(x)
