00e43e02066e9ef428fd10b9114d8d1c69e43da9
Deniz Etkar <denizetkar@gmail.com>
2021-06-28 19:44:03 +0200

    added 'CnnPolicy' to the set of available policies of RecurrentPPO

--- policies/__init__.py
+++ policies/__init__.py
@@ -1,13 +1,15 @@
 from stable_baselines3.common.policies import register_policy
 
-from .actor_critic import ActorCriticCnnRnnPolicy, ActorCriticPolicy, ActorCriticRnnPolicy
+from .actor_critic import ActorCriticCnnPolicy, ActorCriticCnnRnnPolicy, ActorCriticPolicy, ActorCriticRnnPolicy
 
 __all__ = [
+    "ActorCriticCnnPolicy",
     "ActorCriticPolicy",
     "ActorCriticRnnPolicy",
     "ActorCriticCnnRnnPolicy",
 ]
 
 register_policy("MlpPolicy", ActorCriticPolicy)
+register_policy("CnnPolicy", ActorCriticCnnPolicy)
 register_policy("RnnPolicy", ActorCriticRnnPolicy)
 register_policy("CnnRnnPolicy", ActorCriticCnnRnnPolicy)
--- policies/actor_critic.py
+++ policies/actor_critic.py
@@ -391,6 +391,48 @@ class ActorCriticPolicy(BasePolicy):
         self.secondary_extractor.reset_hiddens(batch_size=batch_size)
 
 
+class ActorCriticCnnPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Schedule,
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.PReLU,
+        ortho_init: bool = True,
+        use_sde: bool = False,
+        log_std_init: float = 0.0,
+        full_std: bool = True,
+        sde_net_arch: Optional[List[int]] = None,
+        use_expln: bool = False,
+        squash_output: bool = False,
+        features_extractor_class: Type[BaseFeaturesExtractor] = NatureCNN,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            ortho_init,
+            use_sde,
+            log_std_init,
+            full_std,
+            sde_net_arch,
+            use_expln,
+            squash_output,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+        )
+
+
 class ActorCriticRnnPolicy(ActorCriticPolicy):
     """
     Policy class for actor-critic algorithms (has both policy and value prediction).
